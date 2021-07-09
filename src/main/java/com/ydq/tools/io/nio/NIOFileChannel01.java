c3d762d8f2ed0ad4f5227f8069ad205538049b13
yangdq <1912970306@qq.com>
2021-07-09 18:02:15 +0800

    7.9

---
+++ src/main/java/com/ydq/tools/io/nio/NIOFileChannel01.java
@@ -0,0 +1,33 @@
+package com.ydq.tools.io.nio;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NIOFileChannel01 {
+
+    public static void main(String[] args) throws IOException {
+        String str = "hello,尚硅谷";
+        //创建一个输出流 -> channel
+        FileOutputStream fileOutputStream = new FileOutputStream(".\\file01.txt");
+
+        //通过 fileOutputStream 获取对应的 FileChannel
+        //这个 fileChannel 真实类型是 FileChannelImpl
+        FileChannel fileChannel = fileOutputStream.getChannel();
+
+        //创建一个缓冲区 ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        //将 str 放入 byteBuffer
+        byteBuffer.put(str.getBytes());
+
+        //对 byteBuffer 进行 flip
+        byteBuffer.flip();
+
+        //将 byteBuffer 数据写入到 fileChannel
+        fileChannel.write(byteBuffer);
+        fileOutputStream.close();
+    }
+
+}
